4094ab9254c0f2e28c9a4fff93e2768c9d3a1da9
mac <kkunutsk@gmail.com>
2020-03-28 00:46:23 +0300

    v1.0 - Widget

---
+++ clients.js
@@ -0,0 +1,36 @@
+var clients = [{
+        name: "Михаил Иванович",
+        order: 18795643,
+        discount: 15,
+        company: "RazDvaTri",
+        site: "http://razdvatri.ru"
+    },
+    {
+        name: "Иван Андреевич",
+        order: 17935674,
+        discount: 10,
+        company: "UraUraUra",
+        site: "http://urauraura.ru"
+    },
+    {
+        name: "Семен Семенович",
+        order: 19826374,
+        discount: 5,
+        company: "Trolololo",
+        site: "http://trolololo.ru"
+    },
+    {
+        name: "Валентин Сергеевич",
+        order: 15637289,
+        discount: 20,
+        company: "Lalahey",
+        site: "http://lalahey.ru"
+    },
+    {
+        name: "Петр Ильич",
+        order: 13284563,
+        discount: 25,
+        company: "Younow",
+        site: "http://younow.ru"
+    }
+]
